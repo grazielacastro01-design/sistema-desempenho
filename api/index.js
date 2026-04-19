738bcc46f8159c806290ac9a6916ef4dea1ec038
grazielacastro01-desig <graziela.castro01@aluno.unifametro.edu.br>
2026-04-19 13:22:33 -0300

    ajustando tabela de avaliacao

--- api/index.js
+++ api/index.js
@@ -37,8 +37,8 @@ app.post('/avaliacao', async (req, res) => {
     const { funcionario_id, observacao } = req.body;
     try {
         const dataHoje = new Date().toISOString().split('T')[0];
-        const statusId = 1; 
-        const seuId = 5; // ID da Graziela na tbPessoas
+        const statusId = 1; // Pendente
+        const seuId = 5; // ID da Graziela de Castro
 
         const sql = `
             INSERT INTO tbAvaliacao 
@@ -49,9 +49,10 @@ app.post('/avaliacao', async (req, res) => {
         await db.execute(sql, [dataHoje, observacao, funcionario_id, statusId, seuId]);
         res.status(201).json({ success: true });
     } catch (error) {
+        console.error("Erro no Banco:", error.message);
         res.status(500).json({ error: 'Erro no Banco: ' + error.message });
     }
 });
 
 const port = process.env.PORT || 3000;
-app.listen(port, '0.0.0.0', () => console.log(`Servidor na porta ${port}`));
+app.listen(port, '0.0.0.0', () => console.log(`Servidor rodando na porta ${port}`));
